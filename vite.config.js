47ff76baab6c6d988b37d976d2c22e6c89cbfd2a
Abhishek Negi <negiabhishek579@gmail.com>
2024-09-18 14:33:12 +0530

    Initial commit

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,15 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  base: '/NFT-preview-card-component-FM-challenge-6/', // For GitHub Pages
+  build: {
+    outDir: 'dist',
+    sourcemap: false,
+  },
+  server: {
+    port: 3000,
+    open: true,
+  },
 })
